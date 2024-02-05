c4ad43569e6cb82838412365d315a52b284a4e83
emilharutyunyaNN <harutyunyanemil92@gmail.com>
2024-02-05 23:25:38 +0800

    second commit

--- models.py
+++ models.py
@@ -7,6 +7,7 @@ Base = declarative_base()
 class User(Base):
     __tablename__ = "users"
     id = Column(Integer, primary_key=True)
+    username = Column(String)
     email = Column(String, unique = True, index = True)
     hashed_password = Column(String)
     is_active = Column(Boolean, default = True)
